ca3d09fda20c9fb734ed8ef04355f356a7a21e60
zirot618 <jarl06ab@gmail.com>
2025-04-21 10:06:40 -0500

    ms-asignaturas:conexion base de datos

--- microservicio-asignaturas/main.py
+++ microservicio-asignaturas/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
 from routes import asignatura_router
+from contextlib import asynccontextmanager
+from database import Base, engine
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    Base.metadata.create_all(bind=engine)
+    yield
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 app.title = "Microservicio Gestion de Asignaturas"
 
 
--- microservicio-asignaturas/models/asignatura_model.py
+++ microservicio-asignaturas/models/asignatura_model.py
@@ -1,10 +1,18 @@
 from pydantic import BaseModel
+from sqlalchemy import Column, Integer, String
+from database import Base
 
 class Asignatura(BaseModel):
     id: int
     nombre: str
+    programa: str 
 
-asignaturas = {
-    Asignatura(id=1, nombre="Matematicas"),
-    Asignatura(id=2, nombre="Desarrollo de Software")
-}
+class AsignaturaDB(Base):
+    __tablename__ = "asignaturas"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nombre = Column(String, index=True, nullable=False)
+    programa = Column(String, index=True, nullable=True)
+
+    
+
