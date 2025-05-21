76de5f23156bcba3cf2a6781bb07184af9e6f0a8
Jhon Enriquez <jhonenriquez1998@gmail.com>
2025-05-21 16:39:14 -0500

    feat(deployment): agregar archivos de configuración para el microservicio de horarios

--- backend/microservicio-programas/controllers/repositories/database.py
+++ backend/microservicio-programas/controllers/repositories/database.py
@@ -4,8 +4,6 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from .config import DATABASE_URL
 
 
-
-
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
