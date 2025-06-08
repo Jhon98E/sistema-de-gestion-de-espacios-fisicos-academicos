af7755ced4be5feda4243d45f2e9768bd55c54db
Jhon Enriquez <jhonenriquez1998@gmail.com>
2025-06-08 13:37:48 -0500

    ms-programas: eliminar la ruta de inicio del microservicio de programas

--- backend/microservicio-programas/main.py
+++ backend/microservicio-programas/main.py
@@ -15,7 +15,3 @@ app = FastAPI(lifespan=lifespan, title="Microservicio de Programas academicos")
 
 # Incluir las rutas
 app.include_router(programa_routes.router)
-
-@app.get("/")
-def home():
-    return {"message": "Microservicio de Programas en funcionamiento"}
